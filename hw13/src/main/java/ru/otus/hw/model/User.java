76e0c6394c837c71f362c530b46bd0313f9020e1
artemsamsutdinov <pavlovsckikh@yandex.ru>
2024-09-07 13:51:29 +0400

    change role to authorities

--- hw13/src/main/java/ru/otus/hw/model/User.java
+++ hw13/src/main/java/ru/otus/hw/model/User.java
@@ -1,16 +1,24 @@
 package ru.otus.hw.model;
 
+import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.NamedAttributeNode;
+import jakarta.persistence.NamedEntityGraph;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -19,19 +27,24 @@ import lombok.Setter;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@NamedEntityGraph(name = "user-with-authorities",
+        attributeNodes = @NamedAttributeNode("authorities"))
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @Column(name = "username",nullable = false)
+    @Column(name = "username", nullable = false)
     private String username;
 
-    @Column(name = "password",nullable = false)
+    @Column(name = "password", nullable = false)
     private String password;
 
-    @Column(name = "role")
-    private String role;
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "user_authorities",
+            joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "authority")
+    private Set<String> authorities;
 }
 
--- hw13/src/main/java/ru/otus/hw/repository/UserRepository.java
+++ hw13/src/main/java/ru/otus/hw/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package ru.otus.hw.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.model.User;
@@ -8,5 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @EntityGraph(value = "user-with-authorities", type = EntityGraph.EntityGraphType.FETCH)
     Optional<User> findByUsername(String username);
 }
